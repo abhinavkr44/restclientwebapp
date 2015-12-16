c3e2dd88882d706e74fa87de4ba460576d309250
Abhinav Kumar <abhinav.kumar@hibu.com>
2015-12-16 20:07:09 +0530

    pagination added
    pagination added

--- src/main/webapp/resources/js/test.js
+++ src/main/webapp/resources/js/test.js
@@ -1,5 +1,5 @@
 $(function(){
-	var userApp = angular.module('userApp', []);
+	var userApp = angular.module('userApp', ['angularUtils.directives.dirPagination']);
 	userApp.controller('userDetailController', function($scope) {
 		$.ajax({
 			type: "GET",
@@ -13,24 +13,21 @@ $(function(){
 			error: function(XMLHttpRequest, textStatus, errorThrown) {}
 		});
 	});
-
-	userApp.controller('currenciesController', function($scope) {
-		$.ajax({
-			type: "GET",
-			url: "getListOfCurrencies",
-			async: false,
-			data: {},
-			success: function(data) {
-				console.log(data);
-				var customerInfo = jQuery.parseJSON(data);
-				$scope.listOfCurrencies= customerInfo.currencies;
-				/*$scope.list = $scope.$parent.personList;
-				$scope.config = {
-						itemsPerPage: 5,
-						fillLastPage: true
-				}*/
-			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) {}
+	
+	$("#currencyButton").on("click",function(){
+		userApp.controller('currenciesController', function($scope) {
+			$.ajax({
+				type: "GET",
+				url: "getListOfCurrencies",
+				async: false,
+				data: {},
+				success: function(data) {
+					console.log(data);
+					var customerInfo = jQuery.parseJSON(data);
+					$scope.listOfCurrencies= customerInfo.currencies;
+				},
+				error: function(XMLHttpRequest, textStatus, errorThrown) {}
+			});
 		});
 	});
 	
